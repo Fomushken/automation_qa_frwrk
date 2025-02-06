f5686829e7a83a0cbf84c8e5d5403e5fa2e3f9a8
Marat Fomin <marat.fominn@gmail.com>
2025-02-06 17:42:34 +0400

    implemented generator for data using Faker

---
+++ tests/elements_test.py
@@ -0,0 +1,19 @@
+import time
+
+from pages.elements_page import TextBoxPage
+
+
+class TestElements:
+    class TestTextBox:
+
+        def test_text_box(self, driver):
+            text_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
+            text_box_page.open()
+            input_data = text_box_page.fill_all_fields()
+            output_data = text_box_page.check_filled_form()
+
+            # assert input_data == output_data
+
+            for in_data, out_data in zip(input_data, output_data):
+                assert in_data == out_data f'{in_data} mismatches {out_data}'
+            time.sleep(1)
